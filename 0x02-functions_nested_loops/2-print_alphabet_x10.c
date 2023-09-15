fe676d5f50e9a273e8be9620406fe62f45bad0f1
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2023-09-15 07:13:23 -0700

    fix betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,17 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet then timese
+ *
+ */
+
 void print_alphabet_x10(void)
 {
 int count;
 char c;
-for (count=0;count<=9;count++)
-{	for (c='a';c<='z';c++)
-		_putchar(c);
+for (count = 0 ; count <= 9 ; count++)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+_putchar(c);
 _putchar('\n');
 }
 }
