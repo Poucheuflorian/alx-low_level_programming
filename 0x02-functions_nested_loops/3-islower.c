7aefe6a812a56e72ceca9592995fa64482601fc7
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2023-09-15 06:50:35 -0700

    fix betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,12 @@
+/**
+ * _islower - Determine if is caracter is lowercase
+ * @c: The caracter we want to Know
+ *
+ * Return: 1 if the caracter is lowercase and 0 otherwise
+ */
 int _islower(int c)
 {
 if (c > 96  && c < 123)
-return 1;
-else 
-	return 0;
+return (1);
+	return (0);
 }
