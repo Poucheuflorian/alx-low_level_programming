f2ced68e68da9a3bda4bbbc163edecf93aee2eda
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2023-09-15 06:37:53 -0700

    Betty style fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,14 @@
+
+/**
+ * _isalpha - Determine if a caracter is alphabetic
+ * @c : The caracter
+ *
+ * Return: 1 if is the caracter is alphabetic and 0 otherwise.
+ */
+
 int _isalpha(int c)
 {
-	if ((  c > 64 && c < 91 )||( c > 96 && c < 123 ))
-		return 1;
-	else 
-	return 0;
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
 }
